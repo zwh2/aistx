b3f1d4f8279f2c8c66c97a4e9393a8ea879d5216
bmagistro <koncept1@gmail.com>
2021-02-14 18:35:29 -0500

    make tests optional in cmake; cmake gr module install cleanup

--- python/__init__.py
+++ python/__init__.py
@@ -23,32 +23,13 @@ This is the GNU Radio AISTX module. Place your Python package
 description here (python/__init__.py).
 '''
 
-# ----------------------------------------------------------------
-# Temporary workaround for ticket:181 (swig+python problem)
-import sys
-_RTLD_GLOBAL = 0
+# import swig generated symbols into the satellites namespace
+# this would fail if we are being imported from the build dir
 try:
-    from dl import RTLD_GLOBAL as _RTLD_GLOBAL
+    from .AISTX_swig import *
 except ImportError:
-    try:
-        from DLFCN import RTLD_GLOBAL as _RTLD_GLOBAL
-    except ImportError:
-        pass
-
-if _RTLD_GLOBAL != 0:
-    _dlopenflags = sys.getdlopenflags()
-    sys.setdlopenflags(_dlopenflags|_RTLD_GLOBAL)
-# ----------------------------------------------------------------
-
-
-# import swig generated symbols into the AISTX namespace
-from AISTX_swig import *
+    from AISTX_swig import *
 
 # import any pure python here
 #
 
-# ----------------------------------------------------------------
-# Tail of workaround
-if _RTLD_GLOBAL != 0:
-    sys.setdlopenflags(_dlopenflags)      # Restore original flags
-# ----------------------------------------------------------------
